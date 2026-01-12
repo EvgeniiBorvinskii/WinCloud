06da9a9e7092250f4d870c8d62a2b9379735560b
KeyWest <keywest@example.com>
2026-01-12 13:55:35 -0700

    Fix: Correct PBKDF2 import to PBKDF2HMAC

--- common/crypto.py
+++ common/crypto.py
@@ -6,7 +6,7 @@ import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 import secrets
 
 from common.logger import get_logger
@@ -146,7 +146,7 @@ class CryptoManager:
         if salt is None:
             salt = secrets.token_bytes(16)
         
-        kdf = PBKDF2(
+        kdf = PBKDF2HMAC(
             algorithm=hashes.SHA256(),
             length=32,
             salt=salt,
